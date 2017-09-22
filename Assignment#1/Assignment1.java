0c00f8bb0afa5622f76ebd44993e738d6a5c772e
Dax Henson <dchtexas1@gmail.com>
2017-09-21 19:20:28 -0500

    Begin Added Assignment 1 & 2 files and began working on Assignment 1.

---
+++ Assignment#1/Assignment1.java
@@ -0,0 +1,25 @@
+import java.io.*;
+
+class Assignment1
+{
+    public static void main(String[] args)
+    {
+        #TODO
+    }
+    public static void readFile()
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String line;
+        int lineNum = 0;
+
+        try
+        {
+            while ((line = br.readLine()) != null)
+            {
+
+            }
+            br.close(); 
+        } catch (Exception e) {}
+    }
+}
