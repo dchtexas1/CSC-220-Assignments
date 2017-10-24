72d3cdcc325dd869b59b0e8966de0aa63efe5eeb
Dax Henson <dchtexas1@gmail.com>
2017-10-24 16:21:21 -0500

    Assignment 6-Stacks kinda worked on it. need some clarification as to the stack constructor. will need more clarification in the future.

---
+++ Assignment#6/Stack.java
@@ -0,0 +1,38 @@
+class Stack<datatype> extends List<datatype>
+{
+	public Stack()
+	{
+
+	}
+	public Stack(Stack s)
+	{
+
+	}
+	public void Push(datatype x);
+	{
+
+	}
+	public datatype Pop()
+	{
+		First();
+		GetValue();
+		Remove();
+	}
+	public datatype Peek();
+	{
+		First();
+		GetValue();
+	}
+	public int Size()
+	{
+		GetSize();
+	}
+	public boolean Equals(Stack l);
+	{
+
+	}
+	public Stack<datatype> Add(Stack l);
+	{
+
+	}
+}
